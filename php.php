4dae81ffc5e404c865419c097983de9bd7380496
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-09 13:57:26 -0400

    update original php

--- php.php
+++ php.php
@@ -3,7 +3,7 @@
 // Download the helper library from https://github.com/twilio/authy-php
 // Update the path below to your autoload.php,
 // see https://getcomposer.org/doc/01-basic-usage.md
-require_once './vendor/autoload.php';
+require_once '/path/to/vendor/autoload.php';
 
 $authy_api = new Authy\AuthyApi(getenv('AUTHY_API_KEY'));
 
