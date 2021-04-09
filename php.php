58ecdc81f35d8e30d0f1cad2d3ef6db81a7fed3c
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-09 13:57:08 -0400

    Update PHP to use verify

--- php.php
+++ php.php
@@ -5,22 +5,34 @@
 // see https://getcomposer.org/doc/01-basic-usage.md
 require_once '/path/to/vendor/autoload.php';
 
-$authy_api = new Authy\AuthyApi(getenv('AUTHY_API_KEY'));
+// Find your Account Sid and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+$sid = getenv("TWILIO_ACCOUNT_SID");
+$token = getenv("TWILIO_AUTH_TOKEN");
+$twilio = new Client($sid, $token);
+
+# Create a Verify Service in the Console: https://www.twilio.com/console/verify/services
+$verify_service_sid = getenv("VERIFY_SERVICE_SID");
+
+# Use this instead of the Authy ID.
+# Must be in E.164 format: https://www.twilio.com/docs/glossary/what-e164
+$to_number = "+15017122661";
+
 
 function send() {
-    $sms = $authy_api->requestSms(getenv('AUTHY_ID'));
+    $verification = $twilio->verify->v2->services($verify_service_sid)
+                                   ->verifications
+                                   ->create($to_number, "sms");
 
-    if ($sms->ok()) {
-        printf($sms->message());
-    } else {
-        print_r($sms->errors());
+    if ($verification->status == "pending") {
+        print($verification->sid);
     }
 }
 
 function check($token) {
-    $verification = $authy_api->verifyToken($authy_id, $token);
-
-    if ($verification->ok()) {
-        // correct token
-    }
+    $verification_check = $twilio->verify->v2->services("VAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                                         ->verificationChecks
+                                         ->create($token,
+                                                  ["to" => $to_number]
+                                         );
 }
