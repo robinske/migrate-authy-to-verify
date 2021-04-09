81464453e0c19c8a32ac2a5f6fafe2cbd030fb5e
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-09 13:21:05 -0400

    Update original python version

--- python.py
+++ python.py
@@ -6,9 +6,9 @@ authy_id = os.environ["AUTHY_ID"]
 api_key = os.environ["AUTHY_API_KEY"]
 authy_api = AuthyApiClient(api_key)
 
-sms = authy_api.users.request_sms(authy_id)
+verification = authy_api.users.request_sms(authy_id)
 
-if sms.ok():
-    print(sms.content)
+if verification.ok():
+    print(verification.content)
 else:
-    print(sms.errors())
+    print(verification.errors())
