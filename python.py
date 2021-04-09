98fb47a7d9850ac9381f774cbaae1b5a1812ca6e
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-09 13:22:52 -0400

    rename to client

--- python.py
+++ python.py
@@ -4,9 +4,9 @@ import os
 
 authy_id = os.environ["AUTHY_ID"]
 api_key = os.environ["AUTHY_API_KEY"]
-authy_api = AuthyApiClient(api_key)
+client = AuthyApiClient(api_key)
 
-verification = authy_api.users.request_sms(authy_id)
+verification = client.users.request_sms(authy_id)
 
 if verification.ok():
     print(verification.content)
