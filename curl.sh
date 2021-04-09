4acd2a3f9686d29d93b09068bc5259d4954b6228
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-09 14:10:31 -0400

    Migrate curl example to Verify API

--- curl.sh
+++ curl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Send verification
-curl -i "https://api.authy.com/protected/json/sms/${AUTHY_ID}" \
-    -H "X-Authy-API-Key: ${AUTHY_API_KEY}"
+# phone number in E.164 format https://www.twilio.com/docs/glossary/what-e164
+PHONE_NUMBER="+15017122661"
 
-# Check verification
-curl -i "https://api.authy.com/protected/json/verify/${TOKEN}/${AUTHY_ID}" \
-    -H "X-Authy-API-Key: ${AUTHY_API_KEY}"
+curl -X POST https://verify.twilio.com/v2/Services/$VERIFY_SERVICE_SID/Verifications \
+    --data-urlencode "To=$PHONE_NUMBER" \
+    --data-urlencode "Channel=sms" \
+    -u $TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN
