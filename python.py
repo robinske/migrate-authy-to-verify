a19d2c6b84c449f760f98a7d3174a09bc3a49b51
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-09 13:20:04 -0400

    Python version in Verify

--- python.py
+++ python.py
@@ -1,20 +1,42 @@
-# Download the helper library from https://github.com/twilio/authy-python
-from authy.api import AuthyApiClient
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
 import os
 
-authy_id = os.environ["AUTHY_ID"]
-api_key = os.environ["AUTHY_API_KEY"]
-client = AuthyApiClient(api_key)
+# Find these credentials in the Twilio Console: https://www.twilio.com/console
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+# Create a Verify Service in the Console: https://www.twilio.com/console/verify/services
+verify_service_sid = os.environ['VERIFY_SERVICE_SID']
+
+# Use this instead of the Authy ID.
+# Must be in E.164 format: https://www.twilio.com/docs/glossary/what-e164
+to_number = '+15017122661'
 
 def send():
-    verification = client.users.request_sms(authy_id)
+    try:
+        verification = client.verify \
+            .services(verify_service_sid) \
+            .verifications \
+            .create(to=to_number, channel='sms')
 
-    if verification.ok():
-        print(verification.content)
-    else:
-        print(verification.errors())
+        print(verification.sid)
+    except TwilioRestException as e:
+        print(e)
 
 
 def check(token):
-    verification = authy_api.tokens.verify(authy_id, token=token)
-    print(verification.ok())
+    try:
+        verification_check = client.verify \
+            .services(verify_service_sid) \
+            .verification_checks \
+            .create(to=to_number, code=token)
+
+        if verification_check.status == "approved":
+            # valid token
+        else:
+            # invalid token
+    except TwilioRestException as e:
+        print(e)
