ec928e36e6141af64cecc50697e6c1c2356a0ce2
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-22 18:01:47 -0400

    Add curl check verification

--- curl.sh
+++ curl.sh
@@ -3,7 +3,14 @@
 # phone number in E.164 format https://www.twilio.com/docs/glossary/what-e164
 PHONE_NUMBER="+15017122661"
 
+# Send verification
 curl -X POST https://verify.twilio.com/v2/Services/$VERIFY_SERVICE_SID/Verifications \
     --data-urlencode "To=$PHONE_NUMBER" \
     --data-urlencode "Channel=sms" \
+    -u $TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN
+
+# Check verification
+curl -X POST https://verify.twilio.com/v2/Services/$VERIFY_SERVICE_SID/VerificationCheck \
+    --data-urlencode "To=$PHONE_NUMBER" \
+    --data-urlencode "Code=1234" \
     -u $TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN
