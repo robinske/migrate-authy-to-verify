ed5524cba008b799bb2bcc533b4f845148c54b95
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-22 18:02:46 -0400

    Add original check to curl

--- curl.sh
+++ curl.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Send verification
 curl -i "https://api.authy.com/protected/json/sms/${AUTHY_ID}" \
+    -H "X-Authy-API-Key: ${AUTHY_API_KEY}"
+
+# Check verification
+curl -i "https://api.authy.com/protected/json/verify/${TOKEN}/${AUTHY_ID}" \
     -H "X-Authy-API-Key: ${AUTHY_API_KEY}"
