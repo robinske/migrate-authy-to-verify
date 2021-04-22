0fd8fc3de7b3339b0c8c9be402b7e15b2f354160
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-22 18:00:27 -0400

    Add check statement to Verify ruby

--- ruby.rb
+++ ruby.rb
@@ -8,19 +8,33 @@ account_sid = ENV['TWILIO_ACCOUNT_SID']
 auth_token = ENV['TWILIO_AUTH_TOKEN']
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
+# Create a Verify Service in the Console: https://www.twilio.com/console/verify/services
+verify_service_sid = ENV['VERIFY_SERVICE_SID']
+
+# Use this instead of the Authy ID.
+# Must be in E.164 format: https://www.twilio.com/docs/glossary/what-e164
+to_number = '+15017122661'
+
 
 def send
-    # Create a Verify Service in the Console: https://www.twilio.com/console/verify/services
-    verify_service_sid = ENV['VERIFY_SERVICE_SID']
-
-    # Use this instead of the Authy ID.
-    # Must be in E.164 format: https://www.twilio.com/docs/glossary/what-e164
-    to_number = '+15017122661'
-
     verification = @client.verify
-                        .services(verify_service_sid)
-                        .verifications
-                        .create(to: to_number, channel: 'sms')
+        .services(verify_service_sid)
+        .verifications
+        .create(to: to_number, channel: 'sms')
 
     puts verification.sid
+end
+
+
+def check(token)
+    verification_check = @client.verify
+        .services(verify_service_sid)
+        .verification_checks
+        .create(to: to_number, code: token)
+
+    if verification_check.status == "approved"
+        # correct token
+    else
+        # incorrect token
+    end
 end
