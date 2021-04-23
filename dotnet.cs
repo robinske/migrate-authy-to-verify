c3873020f6680b1f57c90ed9b08cd177a72e8da2
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-23 12:15:26 -0400

    Combine csharp code in one method

--- dotnet.cs
+++ dotnet.cs
@@ -5,13 +5,14 @@ using System.Collections.Generic;
 
 class Program
 {
-    // Your API key from twilio.com/console/authy/applications
-    // DANGER! This is insecure. See http://twil.io/secure
-    var AuthyAPIKey = "your_api_key";
-    var AuthyId = "user's authy id";
-
     static void Main(string[] args)
     {
+        // Your API key from twilio.com/console/authy/applications
+        // DANGER! This is insecure. See http://twil.io/secure
+        var AuthyAPIKey = "your_api_key";
+        var AuthyId = "user's authy id";
+
+        // Send verification
         using (var client = new HttpClient())
         {
             client.DefaultRequestHeaders.Add("X-Authy-API-Key", AuthyAPIKey);
@@ -22,10 +23,8 @@ class Program
             HttpContent responseContent = response.Content;
             Console.WriteLine(responseContent.ReadAsStringAsync().Result);
         }
-    }
 
-    static void Check(string[] args)
-    {
+        // Check verification
         using (var client = new HttpClient())
         {
             client.DefaultRequestHeaders.Add("X-Authy-API-Key", AuthyAPIKey);
